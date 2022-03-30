23100084dc7b69f50749e5c30ec12cb313c6c391
Naman Jain <jain96690@gmail.com>
2022-03-30 12:06:32 +0530

    ui change

--- src/pages/Mails.jsx
+++ src/pages/Mails.jsx
@@ -27,24 +27,44 @@ export default function Mails() {
   return (
     <div className="custom-login-bg">
       <Header />
-      <div className="container mx-auto shadow-lg rounded-md p-4 flex justify-center w-1/2 mt-8">
+      <div className="container-fluid mx-auto shadow-lg rounded-md p-4 flex justify-center w-1/2 mt-8">
         <div>
+          <div className="row gy-5">
           <p className="text-xl text-center mb-4">VERIFY EMAILS</p>
           {providers && (
             <>
               {providers.map((provider, index) => (
-                <List
+    <div className="card text-center col-12 mb-5 border p-5" >
+      {
+        provider.photurl!=""?<img src={provider.photoUrl} className="card-img-top mx-auto" alt="photo"  style={{maxHeight:"400px",minHeight:"250px",maxWidth:"400px"}}/>
+        :<img src="gray.avif" className="card-img-top mx-auto" alt="photo"  style={{maxHeight:"400px",minHeight:"250px",maxWidth:"300px"}}/>
+      }
+    
+    <div className="card-body">
+      <h5 className=" card-title">
+      <List
                   key={index}
                   id={provider.id}
                   getProviders={getProviders}
                   email={provider.email}
-                />
+        />
+        </h5>
+        <ul class="list-group list-group-flush">
+    <li class="list-group-item pb-2">Name: {provider.name}</li>
+    <li class="list-group-item pb-2">Practice: {provider.practice}</li>
+    <li class="list-group-item pb-2">Speciality: {provider.specialty}</li>
+    <li class="list-group-item ">State: {provider.state}</li>
+  </ul>
+   </div>
+  </div>            
+  
               ))}
             </>
           )}
           {!providers && <p>No emails to verify</p>}
         </div>
       </div>
+    </div>
     </div>
   );
 }
